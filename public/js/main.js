5f49ffb925cfcd0c91b2a6a0c2f1b6241954f5fd
thomstrack <satan.skt@gmail.com>
2014-04-21 12:43:38 -0300

    Add project.

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+/* Author : Everton Strack (everton.strack@gmail.com) */      
+
+$html = $('html');
+// Add browser, browser.version, browser.engine and operation system class on html
+$html.browser();
+
+/*
+    document.read
+*/
+$(document).ready(function(){
+	
+});
+
+/*
+    window.load
+*/
+$(window).load(function(){
+	
+});
+ 
