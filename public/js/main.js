d4ddfcac77a194476577a7faa693e4db920b7e9d
Everton Strack <satan.skt@gmail.com>
2014-05-14 16:22:23 -0300

    add Immediately Invoked Function Expression (IIFE)

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,23 @@
 /* Author : Everton Strack (everton.strack@gmail.com) */
 
-// Add browser, browser.version, browser.engine and operation system class on html
-$html = $('html');
-$html.browser();
+(function($, window, document) {
 
-/*
-    document.read
-*/
-$(document).ready(function(){
+	// Add browser, browser.version, browser.engine and operation system class on html
+	$html = $('html');
+	$html.browser();
 
-});
+	/*
+	    document.read
+	*/
+	$(document).ready(function(){
 
-/*
-    window.load
-*/
-$(window).load(function(){
+	});
 
-});
+	/*
+	    window.load
+	*/
+	$(window).load(function(){
+
+	});
+
+}(window.jQuery, window, document));
