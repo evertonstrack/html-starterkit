0df21babd5682ef175b59ab4cdad4dcca9117624
thomstrack <everton.strack@gmail.com>
2016-10-19 21:48:21 -0200

    Code reorganization. Removed some files with the idea of leaving the starter kit the most basic ever.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,65 +1,10 @@
-/* Author : Everton Strack (everton.strack@gmail.com) */
+/* 
+* main.js
+* suthor : Everton Strack (everton.strack@gmail.com)
+**/
+
 (function($, window, document) {
 
-/* ----------------------------------------------------------------------------------------------------------- */
-/* ------------------------------------------------ HELPER --------------------------------------------------- */
-/* ----------------------------------------------------------------------------------------------------------- */
-var Helper = {
-
-	init:function()
-	{
-		Helper.browser();
-	},
-
-	isIE:function()
-	{
-		if(navigator.userAgent.match(/msie|trident/i)){ return true; }
-	},
-
-	browser:function()
-	{
-		var name = bowser.name.toLowerCase();
-		$('html').addClass(name); 
-
-		var version = String(name + Math.floor(bowser.version)).toLowerCase();
-		$('html').addClass(version);
-
-		Modernizr.mobile = bowser.mobile == true || bowser.tablet == true;
-		var mobile = Modernizr.mobile ? 'mobile' : 'no-mobile';
-		$('html').addClass(mobile);
-	}
-}
-
-/* ----------------------------------------------------------------------------------------------------------- */
-/* ------------------------------------------------ MAIN ----------------------------------------------------- */
-/* ----------------------------------------------------------------------------------------------------------- */
-var Main = {
-
-	init:function()
-	{
-		Helper.init();
-	}
-}
-
-
-
-
-/*
-    document.read
-*/
-$(document).ready(function()
-{
-
-	// Main.init
-	Main.init();
-
-});
-
-/*
-    window.load
-*/
-$(window).load(function(){ });
-
-
+var Main = { }
 
 }(window.jQuery, window, document));
